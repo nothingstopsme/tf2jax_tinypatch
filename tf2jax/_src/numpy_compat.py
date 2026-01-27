[tf2jax] Fix `BroadcastArgs` and `Fill` for polymorphic shapes.
The `BroadcastArgs` and `Fill` ops failed when input shapes contained symbolic dimensions.

This CL fixes the issue of `BroadcastArgs` by using `jnp.broadcast_shapes`, which is designed to handle polymorphic shapes during JAX tracing. It fixes the issue of `Fill` by correctly select `jnp` as the backend when the inputs contain symbolic dimension.

PiperOrigin-RevId: 861734224

tf2jax/_src/numpy_compat.py
@@ -89,9 +89,18 @@ def is_poly_dim(x) -> bool:
     return False
 
 
+def _is_np(x):
+  """Checks if `x` is a numpy like type."""
+  # Special case for polymorphic shape tensors. Shape tensors are stored as 1-D
+  # np.array.
+  if isinstance(x, np.ndarray) and x.ndim == 1 and any(map(is_poly_dim, x)):
+    return False
+  return isinstance(x, _NP_LIKES) or is_poly_dim(x)
+
+
 def _get_np(*args):
   """Select numpy backend based on input types."""
-  no_jax = all((isinstance(x, _NP_LIKES) or is_poly_dim(x)) for x in args)
+  no_jax = all(map(_is_np, args))
   return np if no_jax else jnp
 
 
@@ -156,7 +165,7 @@ def empty(shape, dtype: tf.DType, init: bool):
 
 
 def full(shape, fill_value, dtype: tf.DType):
-  dtype = _get_dtypes(shape)[dtype]
+  dtype = _get_dtypes(shape, fill_value)[dtype]
   return _get_np(shape, fill_value).full(shape, fill_value, dtype=dtype)
 
 

tf2jax/_src/ops.py
@@ -454,7 +454,7 @@ def _bit_cast(proto):
 @register_operation("BroadcastArgs")
 def _broadcast_args(proto):
   _check_attrs(proto, {"T"})
-  return lambda s0, s1: np.array(np.broadcast(np.zeros(s0), np.zeros(s1)).shape)
+  return lambda s0, s1: np.array(jnp.broadcast_shapes(s0, s1))
 
 
 class _CaseOp(_HigherOrderFunction):