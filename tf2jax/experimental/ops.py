91986bdaeb818ba54b2e3aa5466b3cbed80062fe
Haifeng Jin <haifengj@google.com>
2026-04-14 11:07:50 -0700

    Change deserialize_portable_artifacts to return an MLIR Module directly rather than forming a string.
    Avoids the need to serialize and deserialize in call_exported.
    
    PiperOrigin-RevId: 899679251

--- tf2jax/experimental/ops.py
+++ tf2jax/experimental/ops.py
@@ -227,10 +227,9 @@ def _xla_call_module(proto):
       return target_platforms.index(jax_backend)
 
   if version >= 4:
-    with mlir.make_ir_context():
-      mhlo_text = str(jex.mlir.deserialize_portable_artifact(
-          proto.attr["module"].s
-      ))
+    mhlo_text = jex.mlir.deserialize_portable_artifact(
+        proto.attr["module"].s
+    )
   else:
     with mlir.make_ir_context():
       module = ir.Module.parse(proto.attr["module"].s)
